33a0f9a4c899d751ff54ea8b2d8f045178624750
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-05-05 15:52:52 +0100

    Update EditBook.jsx

--- src/components/EditBook.jsx
+++ src/components/EditBook.jsx
@@ -10,7 +10,7 @@ const EditBook = () => {
     const {id} = useParams()
 
     useEffect(() => {
-        axios.get('http://localhost:3001/book/book/'+id)
+        axios.get('https://backend-bookstore-nodejs-mongodb.onrender.com/book/book/'+id)
         .then(res => { 
             setName(res.data.name)
             setAuthor(res.data.author)
