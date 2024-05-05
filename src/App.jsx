4e4ddc3828f6f34b0458ce0456bb9518be594e27
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-05-05 15:39:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   axios.defaults.withCredentials = true;
   useEffect(() => {
-    axios.get('http://localhost:3001/auth/verify')
+    axios.get('https://frontend-bookstore-react.onrender.com/auth/verify')
     .then(res => {
       if(res.data.login) {
         setRole(res.data.role)
