ba1d6df27165fc4a69d7723cae3dd3ef333190cc
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-05-05 16:37:21 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,28 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import '../css/Login.css';
-
-const Login = ({ setRoleVar }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('admin');
-  const navigate = useNavigate();
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import '../css/Login.css'
+import axios from 'axios'
+const Login = ({setRoleVar}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('admin')
+  const navigate = useNavigate()
 
   axios.defaults.withCredentials = true;
-  
   const handleSubmit = () => {
-    axios.post('https://backend-bookstore-nodejs-mongodb.onrender.com/auth/login', { username, password, role })
-      .then(res => {
-        if (res.data.login && res.data.role === 'admin') {
-          setRoleVar('admin');
-          navigate('/dashboard');
-        } else if (res.data.login && res.data.role === 'student') {
-          setRoleVar('student');
-          navigate('/');
-        }
-        console.log(res);
-      })
-      .catch(err => console.log(err));
-  };
+    axios.post('http://localhost:3001/auth/login', {username, password, role})
+    .then(res => {
+      if(res.data.login && res.data.role === 'admin') {
+        setRoleVar('admin')
+        navigate('/dashboard')
+      } else if (res.data.login && res.data.role === 'student') {
+        setRoleVar("student")
+        navigate('/')
+      }
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className='login-page'>
@@ -32,27 +30,18 @@ const Login = ({ setRoleVar }) => {
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            placeholder='Enter Username'
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <input type="text" placeholder='Enter Username'
+           onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            placeholder='Enter Password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" placeholder='Enter Password'
+           onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="role">Role:</label>
-          <select
-            name="role"
-            id="role"
-            onChange={(e) => setRole(e.target.value)}
-          >
+          <select name="role" id="role"
+           onChange={(e) => setRole(e.target.value)}>
             <option value="admin">Admin</option>
             <option value="student">Student</option>
           </select>
@@ -60,7 +49,7 @@ const Login = ({ setRoleVar }) => {
         <button className='btn-login' onClick={handleSubmit}>Login</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
